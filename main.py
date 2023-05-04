86b186c599b990711f9f3dcbcdc748adee74bed0
eliyahabba <eliya.habba@mail.huji.ac.il>
2023-05-04 12:42:23 +0300

    project skeleton

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ from CorrelationAnalysis import CorrelationAnalysis
 
 class Main:
     def __init__(self):
-        self.nlp_model = TextModel()
-        self.asr_model = AudioModel()
+        self.text_model = TextModel()
+        self.audio_model = AudioModel()
         self.data_preprocessing = DataPreprocessing()
         self.correlation_analysis = CorrelationAnalysis()
 
     def run(self):
-        # code for running the project, including the interface
         pass
 
 if __name__ == '__main__':
