4e1e6069b8bb613991b8d1a94603e080a0bfe3b8
eliyahabba <eliya.habba@mail.huji.ac.il>
2023-05-08 18:38:21 +0300

    added option to choose attention_layer. Use the datamodels

--- AttentionExtractor/TextAttentionExtractor.py
+++ AttentionExtractor/TextAttentionExtractor.py
@@ -1,35 +1,37 @@
 import torch
-from transformers import AutoModelForMaskedLM, AutoTokenizer
 
 from AttentionExtractor.AttentionExtractor import AttentionExtractor
+from DataModels.TextModel import TextModel
 
 
 class TextAttentionExtractor(AttentionExtractor):
+    """
+    This class is responsible for extracting the attention matrices from the text model.
+    """
     def __init__(self, model_name: str):
         # use super() to call the parent class constructor
         super().__init__(model_name)
-        self.model = AutoModelForMaskedLM.from_pretrained(self.model_name, output_attentions=True)
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.text_model = TextModel(model_name)
 
     def run_model(self, text):
         # Tokenize the input text with both models
-        tokens = self.tokenizer.tokenize(text)
+        tokens = self.text_model.tokenizer.tokenize(text)
 
         # TODO: check if we need to add special tokens for BERT
         # Add special tokens for BERT
         # bert_tokens = ['[CLS]'] + bert_tokens + ['[SEP]']
 
         # Convert the tokens to input IDs for both models
-        input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
+        input_ids = self.text_model.tokenizer.convert_tokens_to_ids(tokens)
 
         # Convert the input IDs to PyTorch tensors
         input_ids = torch.tensor(input_ids).unsqueeze(0)
 
         # Get the outputs and attention matrices for both models
-        outputs = self.model(input_ids)
+        outputs = self.text_model.model(input_ids)
         return outputs
 
-    def extract_attention(self, text, head):
+    def extract_attention(self, text, attention_layer, head):
         outputs = self.run_model(text)
-        attentions = self.get_attention_matrix(outputs, head)
+        attentions = self.get_attention_matrix(outputs, attention_layer, head)
         return attentions
