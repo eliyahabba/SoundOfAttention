f2853536eb932ff369b93f7fd0be11cbb3cfcd0e
eliyahabba <eliya.habba@mail.huji.ac.il>
2023-05-08 19:39:25 +0300

    refactor to AttentionsComparators

--- AttentionsComparators/AttentionsComparator.py
+++ AttentionsComparators/AttentionsComparator.py
@@ -6,16 +6,15 @@ class AttentionsComparator(ABC):
     Abstract class for comparing attention weights.
     """
 
-    # @abstractmethod
-    # def run_models(self, text, audio=None):
-    #     """
-    #     Run the models to get the attention weights.
-    #     """
-    #     pass
+    @abstractmethod
+    def create_attention_matrices(self, text, audio=None):
+        pass
+
     #
     # @abstractmethod
     # def generate_insights(self, correlation_matrix):
     #     pass
 
+    # @abstractmethod
     def compare_attention_matrices(self, model1_attention_matrix, model2_attention_matrix):
         pass
