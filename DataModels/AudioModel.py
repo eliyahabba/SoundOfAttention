c7462b0909fbaf8556117b7ca7d9a0440a4fdae1
eliyahabba <eliya.habba@mail.huji.ac.il>
2023-05-19 09:24:34 +0300

    Removed tokenizer. Added hint types.

--- DataModels/AudioModel.py
+++ DataModels/AudioModel.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
-from transformers import Wav2Vec2Tokenizer, Wav2Vec2Model
+import torch
+from transformers import Wav2Vec2Model, Wav2Vec2Processor
 
 from Common.Constants import Constants
 
@@ -16,6 +17,7 @@ class AudioModel:
         model_name (str): The name of the Audio model
     """
     model_name: str = AudioConstants.W2V2
+    device: torch.device = torch.device('cpu')
 
     def __post_init__(self):
         """
@@ -23,12 +25,14 @@ class AudioModel:
         """
 
         # Initialize the tokenizer and model
-        tokenizer_class, model_class = self._get_audio_model_and_tokenizer(self.model_name)
-        self.tokenizer = tokenizer_class.from_pretrained(self.model_name)
+        model_class, processor_class = self._get_audio_model_and_tokenizer(self.model_name)
         self.model = model_class.from_pretrained(self.model_name, output_attentions=True)
+        self.processor = processor_class.from_pretrained(self.model_name)
+        # Move the model to the provided device
+        self.model.to(self.device)
 
     @staticmethod
-    def _get_audio_model_and_tokenizer(model_name_or_path):
+    def _get_audio_model_and_tokenizer(model_name_or_path: str) -> (Wav2Vec2Model, Wav2Vec2Processor):
         """
         Get the audio model and tokenizer based on the provided model name or path
         :param model_name_or_path: The name or path of the audio model
@@ -36,8 +40,8 @@ class AudioModel:
         """
         # Map the model name to the corresponding tokenizer and model class
         if model_name_or_path == AudioConstants.W2V2:
-            tokenizer_class = Wav2Vec2Tokenizer
             model_class = Wav2Vec2Model
+            processor_class = Wav2Vec2Processor
         else:
             raise ValueError("Unknown audio model:", model_name_or_path)
-        return tokenizer_class, model_class
+        return model_class, processor_class
