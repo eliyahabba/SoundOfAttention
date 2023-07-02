72f78aed10dcc92d4bc1fbd37ed043799bb0f7fc
eliyahabba <eliya.habba@mail.huji.ac.il>
2023-07-02 21:19:18 +0300

    added documentation to sample class

--- DataModels/Sample.py
+++ DataModels/Sample.py
@@ -4,5 +4,15 @@ from typing import Union
 
 @dataclass
 class Sample:
+    """
+    This class represents a sample to our project.
+    Each sample has a text and can have an audio, but not necessarily.
+
+    :param text: The text of the sample.
+    :param audio: (Optional).
+    The audio of the sample. The audio is a dictionary with the following keys:
+    'array' (np.ndarray), 'sampling_rate' (int) ect.
+
+    """
     text: Union[str, dict]
     audio: dict = None
